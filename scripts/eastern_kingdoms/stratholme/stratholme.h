b6f7ee04ff333b7dd880ee366c1bf8549430122c
Charles A Edwards <charles.a.edwards@btinternet.com>
2015-01-21 15:40:25 +0000

    Strat - The Unforgiven script code correction
    The Unforgiven (stratholme) - identifier initialisation of
    m_bTheUnforgivenSpawnHasTriggered moved to instance_stratholme.cpp

--- scripts/eastern_kingdoms/stratholme/stratholme.h
+++ scripts/eastern_kingdoms/stratholme/stratholme.h
@@ -223,7 +223,7 @@ class instance_stratholme : public ScriptedInstance
         GuidList m_luiGuardGUIDs;
 
         // this ensures that the code that deals with the initial spawning of The Unforgiven and its adds (Vengful Phantoms) is only run once
-        bool bTheUnforgivenSpawnHasTriggered = false;
+        bool m_bTheUnforgivenSpawnHasTriggered;
 };
 
 #endif
