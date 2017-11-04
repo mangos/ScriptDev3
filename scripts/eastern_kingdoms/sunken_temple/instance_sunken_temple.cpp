606a418b4d22201660fe0153dae99f7af543eb02
bdebaere <brechtdebaere@yahoo.com>
2017-11-04 08:55:11 +0100

    Update instance_sunken_temple.cpp

--- scripts/eastern_kingdoms/sunken_temple/instance_sunken_temple.cpp
+++ scripts/eastern_kingdoms/sunken_temple/instance_sunken_temple.cpp
@@ -1,36 +1,36 @@
 /**
-* ScriptDev3 is an extension for mangos providing enhanced features for
-* area triggers, creatures, game objects, instances, items, and spells beyond
-* the default database scripting in mangos.
-*
-* Copyright (C) 2006-2013  ScriptDev2 <http://www.scriptdev2.com/>
-* Copyright (C) 2014-2017  MaNGOS  <https://getmangos.eu>
-*
-* This program is free software; you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation; either version 2 of the License, or
-* (at your option) any later version.
-*
-* This program is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-* GNU General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License
-* along with this program; if not, write to the Free Software
-* Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-*
-* World of Warcraft, and all World of Warcraft or Warcraft art, images,
-* and lore are copyrighted by Blizzard Entertainment, Inc.
+ * ScriptDev3 is an extension for mangos providing enhanced features for
+ * area triggers, creatures, game objects, instances, items, and spells beyond
+ * the default database scripting in mangos.
+ *
+ * Copyright (C) 2006-2013  ScriptDev2 <http://www.scriptdev2.com/>
+ * Copyright (C) 2014-2017  MaNGOS  <https://getmangos.eu>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * World of Warcraft, and all World of Warcraft or Warcraft art, images,
+ * and lore are copyrighted by Blizzard Entertainment, Inc.
 */
 
 /**
-* ScriptData
-* SDName:      instance_sunken_temple
-* SD%Complete: 90
-* SDComment:   Hakkar Summon Event needs more sources to improve
-* SDCategory:  Sunken Temple
-* EndScriptData
+ * ScriptData
+ * SDName:      instance_sunken_temple
+ * SD%Complete: 90
+ * SDComment:   Hakkar Summon Event needs more sources to improve
+ * SDCategory:  Sunken Temple
+ * EndScriptData
 */
 
 #include "precompiled.h"
